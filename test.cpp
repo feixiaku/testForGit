64be948585bc17a6d2bc0039d09a07a8074f863c
wangl <feixiaku@gmail.com>
2016-10-26 11:11:22 +0800

    add test.cpp

---
+++ test.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: test.cpp
+	> Author: wangl
+	> Mail: feixiaku@gmail.com
+	> Created Time: Wed Oct 26 11:03:53 2016
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main(int argc, char* argv[])
+{
+    cout << "Hello git" << endl;
+    return 0;
+}
+
